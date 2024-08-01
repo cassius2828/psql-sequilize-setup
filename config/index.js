b3bbc4ce735dc6be2f4ae91a71ef451da6c88300
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-01 10:39:27 -0700

    updated to organize code

--- config/index.js
+++ config/index.js
@@ -5,9 +5,6 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-// const host =
-//   process.env.NODE_ENV === "production" ? process.env.HOST : "localhost";
-
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host:'localhost',
   dialect: "postgres",
@@ -19,15 +16,7 @@ const Exhibition = require('../models/exhibition')(sequelize, Sequelize.DataType
 const Artwork = require('../models/artwork')(sequelize, Sequelize.DataTypes);
 
 
-
 applyExtraSetup(sequelize);
-// // Run `.associate` if it exists
-// Object.keys(models).forEach(modelName => {
-//   if ('associate' in models[modelName]) {
-//     models[modelName].associate(models);
-//   }
-// });
 
-// sequelize.models = models;
 
 module.exports = sequelize;
--- models/user.js
+++ models/user.js
@@ -1,28 +1,25 @@
 // models/User.js
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-    });
-  
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
 
-  
-    return User;
-  };
-  
+  return User;
+};
